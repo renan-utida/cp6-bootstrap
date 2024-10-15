b6ca4c50233a40db99b56a7495186fbbf8e4a9be
Renan <renandiutida@gmail.com>
2024-10-14 21:18:49 -0300

    Incluindo titulos e descricoes para as imagens do carousel

--- src/components/CarouselsImagens.jsx
+++ src/components/CarouselsImagens.jsx
@@ -17,68 +17,68 @@ function CarouselsImagens() {
       <Carousel.Item className="carousel-img object-fit-cover">
         <img src={CamisetaCarousel1} alt="Camisetas - Marvel" className='h-100 w-100'/>
         <Carousel.Caption className='descricao'>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h3>Camisetas Marvel na Promoção</h3>
+          <p>Venha conferir! Camisetas por até 50% de desconto!</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item className="carousel-img object-fit-cover">
         <img src={ChaveiroCarousel1} alt="Chaveiros - DC Comics" className='w-100 h-100'/>
         <Carousel.Caption className='descricao'>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h3>Últimos Chaveiros da DC disponíveis!</h3>
+          <p>Venha garantir o seu antes que acabe!</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item className="carousel-img object-fit-cover">
         <img src={MiniaturaCarousel1} alt="Miniaturas Funko Pop - Marvel, Vingadores" className='w-100 h-100'/>
         <Carousel.Caption className='descricao'>
-          <h3>Third slide label</h3>
+          <h3>Miniaturas Marvel Funko Pop com Frete Grátis!</h3>
           <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          Colecione todos os seus heróis favoritos! Frete grátis, não perca!
           </p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item className="carousel-img object-fit-cover">
         <img src={RevistasCarousel1} alt="Revistas diversos Animes" className='w-100 h-100'/>
         <Carousel.Caption className='descricao'>
-          <h3>Third slide label</h3>
+          <h3>Revistas de diversos animes com Ofertas Imperdíveis!</h3>
           <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            Mergulhe no mundo dos Animes! Histórias emocionantes e artes incríveis em cada edição!
           </p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item className="carousel-img object-fit-cover">
         <img src={CamisetaCarousel2} alt="Camisetas - Naruto" className='w-100 h-100'/>
         <Carousel.Caption className='descricao'>
-          <h3>Third slide label</h3>
+          <h3>Camisetas do Naruto - Apenas hoje com 30% OFF!</h3>
           <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            Venha aproveitar essa promoção de camisetas do Naruto para você se destacar!
           </p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item className="carousel-img object-fit-cover">
         <img src={ChaveiroCarousel2} alt="Chaveiros de personagens da Marvel" className='w-100 h-100'/>
         <Carousel.Caption className='descricao'>
-          <h3>Third slide label</h3>
+          <h3>Chaveiros Marvel - Apenas Hoje com 30% OFF!</h3>
           <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          Carregue seus personagens favoritos para onde for!
           </p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item className="carousel-img object-fit-cover">
         <img src={MiniaturaCarousel2} alt="Funko Pop - Dc Comics, Liga da Justiça" className='w-100 h-100'/>
         <Carousel.Caption className='descricao'>
-          <h3>Third slide label</h3>
+          <h3>Miniaturas DC Comics - Desconto Especial para Colecionadores!</h3>
           <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          Junte os maiores heróis do universo! Adquira o seu Funko Pop agora mesmo!
           </p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item className="carousel-img object-fit-cover">
         <img src={RevistasCarousel2} alt="Revistas - Marvel" className='w-100 h-100'/>
         <Carousel.Caption className='descricao'>
-          <h3>Third slide label</h3>
+          <h3>Nova coleção de revistas da Marvel - 2024/2025</h3>
           <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          Venha comprar a nova coleção! Relíquias de histórias épicas que moldaram o universo Marvel!
           </p>
         </Carousel.Caption>
       </Carousel.Item>
